cedafc501081e0c15a373f062354194b0fde0d35
samabcde <tszsumng90@gmail.com>
2023-04-15 23:51:36 +0800

    upgrade dependency

--- src/test/java/com/samabcde/analyse/engine/execute/ExecutorExceptionHandlerTest.java
+++ src/test/java/com/samabcde/analyse/engine/execute/ExecutorExceptionHandlerTest.java
@@ -1,11 +1,10 @@
 package com.samabcde.analyse.engine.execute;
 
-import com.samabcde.analyse.engine.execute.AnalyseProcessState;
-import com.samabcde.analyse.engine.execute.ExecutorExceptionHandler;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class ExecutorExceptionHandlerTest {
     private final AnalyseProcessState analyseProcessState = new AnalyseProcessState();
@@ -15,6 +14,7 @@ class ExecutorExceptionHandlerTest {
     @Test
     public void GivenExceptionHandler_WhenExceptionUncaught_ShouldFlagProcessStateErrorOccur() {
         assertThat(analyseProcessState.isErrorOccur()).isFalse();
+        when(exception.getSuppressed()).thenReturn(new Throwable[]{});
         ExecutorExceptionHandler exceptionHandler = new ExecutorExceptionHandler(analyseProcessState);
         exceptionHandler.uncaughtException(thread, exception);
         assertThat(analyseProcessState.isErrorOccur()).isTrue();
